2a104483a8b3083f4310f0c27fc4de3ea7160692
Fred-Victor-YANG <frederic.victor.yang@gmail.com>
2020-09-30 16:07:12 +0200

    merging

--- storeDevis.php
+++ storeDevis.php
@@ -2,24 +2,24 @@
     require 'conn.php';
 
     $data = $_POST;
-    $prinProduits = $data['radio'];
+    $mainProduct = $data['radio'];
     //autreproduit
     $checkbox = $_POST['checkbox'];   
     for($i=0;$i<count($checkbox);$i++){
-        $autreProduits= $autreProduits.$checkbox[$i]."  ";  
+        $otherproduct= $otherproduct.$checkbox[$i]."  ";  
     }
-    $societe = $data['societe']; 
+    $company = $data['company']; 
     $email = $data['email'];
 
     $telephone = $data['telephone'];
-    $codePostale = $data['codePostale'];
-    $addresse = $data['addresse'];
+    $postcode = $data['postcode'];
+    $address = $data['address'];
     $messages = $data['message'];
 
     echo "电话：".$telephone;
 
     $sql = "INSERT INTO Devis (prinProduits, autreProduits, societe, email, telephone, codePostale, addresse, messages, statut) 
-    VALUES ('$prinProduits', '$autreProduits', '$societe', '$email','$telephone','$codePostale','$addresse','$messages',0);";
+    VALUES ('$mainProduct', '$otherproduct', '$company', '$email','$telephone','$postcode','$address','$messages',0);";
     
     if ($conn->query($sql) === TRUE) {
         echo "新记录插入成功";
